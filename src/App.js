34ab5f08c9bc348ff25ad578c9954c2bd79373c0
faz.pak@gmail.com <kb337137@gmail.com>
2021-02-15 18:09:20 +0500

    complet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+// import { useState, useEffect } from 'react';
+import React, {useState,useEffect} from 'react'
 
 function App() {
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get(`https://www.reddit.com/r/reactjs.json`)
+      .then(res => {
+        const newPosts = res.data.data.children
+          .map(obj => obj.data);
+
+        setPosts(newPosts);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>reactjs</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+
+    </>
   );
 }
 
 export default App;
+
